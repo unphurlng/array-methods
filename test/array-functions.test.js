308eea7de30fa1fdfcfd1b80b737574519c90500
heather <unphurlng@gmail.com>
2019-04-01 17:01:53 -0700

    tests passing for map

--- test/array-functions.test.js
+++ test/array-functions.test.js
@@ -2,11 +2,17 @@ const { mapArray } = require('../lib/array-functions');
 
 describe('array functions', () => {
   describe('map array function', () => {
-    it('takes array, creates new array with callback value', () => {
+    it('takes array, copies original array with callback value', () => {
       const mock = jest.fn(num => num);
       expect(mapArray([1, 2, 3, 4, 5], mock)).toEqual([1, 2, 3, 4, 5]);
+    });
+    
+    it('takes array, ignores hole in the array and returns a copy with the hole', () => {
+      const mock = jest.fn(num => num + 1);
+      expect(mapArray([1, 2, 3,, 4, 5], mock)).toEqual([2, 3, 4,, 5, 6]);
     });
   });
 
 
 });
+
